acadd93fe8ca1a9f33833262f0b34acd9167a546
Aidan <aidan.russell@stonybrook.edu>
2020-12-01 14:58:53 -0500

    more cleanup

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ https://dinkieshy.github.io/Geometry-Madness/
 
 # Controls:
 Double click to add a point  
-Click a point to select it, then click another to make a line between them  
+Right click (and drag) to select point(s)
+Click and hold a point to drag selected points, or if over a point, add an edge. Drag the edge to another point to connect them!
 Do the same to two points that are already connected to remove the line  
-Click and drag to move points
--- scripts/neatfunctions.js
+++ scripts/neatfunctions.js
@@ -1,34 +1,4 @@
-
-function selection(shape, selector, selection, single) {   // select one or more points from the canvas
-    let selection = [];
-
-    for(const point in shape.points) {
-        // single point select (click)
-        if(single) {
-            if(point.contains(x, y)) {		// if mouse is within point
-                selection.push(point);
-                point.fill = FILLS.SELECTED_FILL;
-                point.dragging = true;
-            }
-        }
-
-        // multi point select (click + drag)
-    }
-}
-
-function pointInBox(point, box) {
-    //return ()
-}
-
-function selectInBox(shape, box) {
-    let selection = [];
-
-    for(const point in shape.points) {
-        
-    }
-}
-
-function generateKGon(k, canvas, shape) {
+function generateKGon(k, canvas, shape) {   // todo
 
     let center = {
         x: canvas.width / 2,
